d767b6b05cf117a600a9c965d98518ec7fc266b2
kay Warrie <kaywarrie@hotmail.com>
2022-03-18 14:42:32 +0100

    added dl button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,53 +2,61 @@
 import logo from '../../img/logo.svg';
 //js
 import { Component } from 'react';
+import { download, geocode_adres } from './tools'
 import papa from 'papaparse';
 //css
 import './App.css';
 
-const geopuntURI = "https://loc.geopunt.be/v4/Location"
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {file_name: 'Kies een csv-file', rows: [], columns: []};
+    this.state = {file_name: '', rows: [], columns: []};
     this.handleNewFile = this.handleNewFile.bind(this);
     this.processCsv =  this.processCsv.bind(this);
-    this.geocode_adres =  this.geocode_adres.bind(this);
-    this.onCellChange =  this.onCellChange.bind(this);
+    this.geocode_adres = this.geocode_adres.bind(this);
+    this.onCellChange = this.onCellChange.bind(this);
+    this.download_csv = this.download_csv.bind(this);
   }
  
   async geocode_adres(){
     let huisnr = document.getElementById('huisnr').value ;
-    let straatnaam = document.getElementById('straatnaam').value ;
-    let pc_gemeente =  document.getElementById('pc_gemeente').value ;
+    let straat = document.getElementById('straatnaam').value ;
+    let pc =  document.getElementById('pc_gemeente').value ;
     let rows = this.state.rows;
     for  (let idx = 0; idx < rows.length; idx++) {
         let row = rows[idx].data
-        let adres = `${row[straatnaam]}  ${row[huisnr]}, ${row[pc_gemeente]}`;
-        let result = await fetch( `${geopuntURI}?q=${adres}`).then(e => e.json());
-        let loc = result.LocationResult;
-        console.log(`${geopuntURI}?q=${adres}`);
-        if( loc.length > 0){
-          let x= loc[0].Location.X_Lambert72;
-          let y= loc[0].Location.Y_Lambert72;
+        let adres = `${straat !='_'? row[straat]:''} ${huisnr !='_'? row[huisnr]:''} ${pc !='_'? ', '+ row[pc]:''}`;
+        let loc = await geocode_adres(adres);
+        if( loc != null){
+          let x= loc.Location.X_Lambert72;
+          let y= loc.Location.Y_Lambert72;
+          rows[idx].data.status = loc.LocationType;
           rows[idx].data.x = x;
           rows[idx].data.y = y;
         }
         else{
           rows[idx].data.x = -1;
           rows[idx].data.y = -1;
+          rows[idx].data.status = 'Niet gevonden';
         }
     }
     this.setState({rows: rows })
   }
 
+  download_csv(){
+    if(this.state.file_name == ''){return}
+    let rows =  this.state.rows.map(e => e.data);
+    let fname = "XY"+ this.state.file_name;
+    download( fname, papa.unparse( rows , {delimiter: ";",}) );
+  }
+
   processCsv( csv ){
-    let xyCols = ["x", "y"].concat( Object.keys(csv.data[0]) );
+    let xyCols = ["x", "y", "status"].concat( Object.keys(csv.data[0]) );
     let cols = xyCols.map((e, i) => ({id: `head${i}`, value: e}) );
 
     let rows = csv.data.map((e, i) => { 
-      let row = Object.assign({x: '', y: ''}, e);
+      let row = Object.assign({ x: '', y: '', status: ''}, e);
       return {id: `row${i}`, data: row}
     });
     this.setState({columns: cols});
@@ -90,6 +98,7 @@ class App extends Component {
           <br/>
           <label htmlFor="straatnaam">Straatnaam:&nbsp;</label>
           <select name="straatnaam" id="straatnaam" >
+          <option key="straatnaam_blanc" value='_'>&lt;geen&gt;</option>
             {this.state.columns.map( o => {
               return <option key={"straatnaam"+o.id}  value={o.value}>{o.value}</option>
             })}
@@ -97,6 +106,7 @@ class App extends Component {
           <br/>
           <label htmlFor="huisnr">Huisnummer:&nbsp;</label>
           <select name="huisnr" id="huisnr" >
+          <option key="huisnr_blanc" value='_'>&lt;geen&gt;</option>
             {this.state.columns.map( o => {
               return <option key={"huisnr"+o.id} value={o.value}>{o.value}</option>
             })}
@@ -104,28 +114,34 @@ class App extends Component {
           <br/>
           <label htmlFor="pc_gemeente">Postcode of Gemeente:&nbsp;</label>
           <select name="pc_gemeente" id="pc_gemeente" >
+          <option key="pc_gemeente_blanc" value='_'>&lt;geen&gt;</option>
             {this.state.columns.map( o => {
               return <option key={"pc_gemeente"+o.id} value={o.value}>{o.value}</option>
             })}
           </select>
         </div>
         <button onClick={this.geocode_adres}>Geocodeer</button>
+        <button onClick={this.download_csv}>Download </button>
          <table id="main-table" >
          <tbody>
            <tr key='head'>
+             <th  key="checkbox"></th>
           {this.state.columns.map( o => {
-						return <th  key={o.id}>{o.value}</th>
+						return <th  key={o.id}><div>{o.value}</div></th>
 				  })}
            </tr>
           {this.state.rows.map((row, rowIdx) => {
               let values = Object.values(row.data);
-              return <tr key={row.id} >  
+              return (<tr key={row.id} >  
+              <td><input type="checkbox" /></td>
               {values.map( (cell,cellIdx) => (
                 <td key={`${row.id}-${cellIdx}`} >
-                  <input onChange={() => {this.onCellChange(`cell-${row.id}-${cellIdx}`, rowIdx, cellIdx)}}
-                    type="text" id={`cell-${row.id}-${cellIdx}`} value={cell}/>
+                  <input  disabled = {cellIdx < 3 ? "disabled" : ""}
+                   className={ row.data.x > 0 ? "found" : row.data.x == '' ? '': "notfound" }
+                   onChange={() => {this.onCellChange(`cell-${row.id}-${cellIdx}`, rowIdx, cellIdx)}}
+                   type="text" id={`cell-${row.id}-${cellIdx}`} value={cell}/>
                 </td>
-              ))} </tr>
+              ))} </tr>)
             })}
          </tbody>
          </table>
---
+++ src/components/App/tools.js
@@ -0,0 +1,22 @@
+
+
+
+async function geocode_adres(adres){
+  const geopuntURI = "https://loc.geopunt.be/v4/Location";
+  let r = await fetch( `${geopuntURI}?q=${adres}`);
+  let rjs = await r.json();
+  return rjs.LocationResult.length > 0 ? rjs.LocationResult[0] : null;
+}
+
+
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
+
+export {download, geocode_adres };
